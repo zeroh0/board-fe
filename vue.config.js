abe2f5efbf3c088a22a19dfbf76870d79ad32000
zeroh0 <umberto1900@naver.com>
2022-10-09 23:08:54 +0900

    feat: spring boot 프로젝트 연동

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: '../board-be/src/main/resources/static',
+  devServer: {
+    proxy: {
+      'api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
+  }
 })
